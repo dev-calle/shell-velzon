d7ec0719a2af867dae12b7fadc60a3980135d433
Erick Yenque <erickyeme9@gmail.com>
2023-05-04 14:35:59 -0500

    fix: footer title app

--- src/app/layouts/shared/footer/footer.component.ts
+++ src/app/layouts/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { APP_NAME } from 'src/app/constants/app.constants';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  title = APP_NAME;
 
   year: number = new Date().getFullYear();
 
